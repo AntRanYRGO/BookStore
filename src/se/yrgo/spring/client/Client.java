9bf7cf86ebd66993c97f694a68d1c741fdcd0a14
AntRanYRGO <antran0410@skola.goteborg.se>
2022-12-20 22:31:49 +0100

    Finishing workshop 13-Spring-Transactions-Workshop

--- src/se/yrgo/spring/client/Client.java
+++ src/se/yrgo/spring/client/Client.java
@@ -15,21 +15,25 @@ public class Client {
 		ClassPathXmlApplicationContext container
 				= new ClassPathXmlApplicationContext("application.xml");
 
-		BookService bookService = container.getBean(BookService.class);
-		bookService.registerNewBook(new Book("1234596896812", "Birds",
-				"Bird Lover", 100.00));
-		List<Book> allBooks = bookService.getEntireCatalogue();
-		for (Book book:allBooks) {
-		System.out.println(book);
-	}
-
 		try {
-			Book findBook=bookService.getBookByIsbn("1234596896812");
-			System.out.println(findBook);
-		}catch (BookNotFoundException e) {
-			System.err.println("Sorry, that book does not exist");
-		}
-		container.close();
+			BookService bookService = container.getBean(BookService.class);
 
+			bookService.registerNewBook(new Book("1234596896812", "Birds",
+					"Bird Lover", 100.00));
+
+			List<Book> allBooks = bookService.getEntireCatalogue();
+			for (Book book : allBooks) {
+				System.out.println(book);
+			}
+		} finally {
+			container.close();
+		}
+
+//		try {
+//			Book findBook=bookService.getBookByIsbn("1234596896812");
+//			System.out.println(findBook);
+//		}catch (BookNotFoundException e) {
+//			System.err.println("Sorry, that book does not exist");
+//		}
 	}
 }
