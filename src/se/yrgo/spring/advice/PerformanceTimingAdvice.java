5f69632bd7e50fd10c060ddedbd07db3842e609b
AntRanYRGO <antran0410@skola.goteborg.se>
2022-12-27 12:52:49 +0100

    Working on wshop12

--- src/se/yrgo/spring/advice/PerformanceTimingAdvice.java
+++ src/se/yrgo/spring/advice/PerformanceTimingAdvice.java
@@ -2,10 +2,11 @@ package se.yrgo.spring.advice;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.ProceedingJoinPoint;
 
-public class PerformanceTimingAdvice implements MethodInterceptor {
-    @Override
-    public Object invoke(MethodInvocation method) throws Throwable {
+public class PerformanceTimingAdvice {
+
+    public Object  performTimingMeasurement (ProceedingJoinPoint method) throws Throwable{
         //before
         long startTime = System.nanoTime();
 
@@ -17,9 +18,7 @@ public class PerformanceTimingAdvice implements MethodInterceptor {
             //after
             long endTime = System.nanoTime();
             long timeTaken = endTime - startTime;
-            System.out.println("The method " +
-                    method.getMethod().getName() + " took " + timeTaken
-                    / 1000000 + "ms");
+            System.out.println("The method " + method.getSignature().getName() + " took " + timeTaken/1000000);
         }
     }
 }
